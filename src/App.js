24a9d248060e2d2cb2d58675df0bdb5390ff77e3
Amajuoyi chidera <amajuoyichideradavid@gmail.com>
2023-09-08 14:33:30 +0100

    first version

--- src/App.js
+++ src/App.js
@@ -1,25 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css"; 
 
 function App() {
+  const [budget, setBudget] = useState(0);
+  const [expense, setExpense] = useState("");
+  const [amount, setAmount] = useState("");
+  const [expenses, setExpenses] = useState([]);
+
+
+  const addExpense = () => {
+    if (expense.trim() === "" || isNaN(amount) || parseFloat(amount) <= 0) {
+      return;
+    }
+
+    const newExpense = {
+      id: Date.now(),
+      name: expense,
+      amount: parseFloat(amount),
+    };
+
+    setExpenses([...expenses, newExpense]);
+    setBudget(budget - newExpense.amount);
+    setExpense("");
+    setAmount("");
+  };
+
+
+  const deleteExpense = (id) => {
+    const updatedExpenses = expenses.filter((expense) => expense.id !== id);
+    const deletedExpense = expenses.find((expense) => expense.id === id);
+    if (deletedExpense) {
+      setBudget(budget + deletedExpense.amount);
+    }
+    setExpenses(updatedExpenses);
+  };
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Budget App</h1>
+
+      <div className="budget">
+        <h2>Your Budget: ${budget.toFixed(2)}</h2>
+        <input
+          type="number"
+          placeholder="Enter Your Budget"
+          value={budget}
+          onChange={(e) => setBudget(parseFloat(e.target.value))}
+        />
+      </div>
+
+      <div className="expense-input">
+        <input
+          type="text"
+          placeholder="Expense"
+          value={expense}
+          onChange={(e) => setExpense(e.target.value)}
+        />
+        <input
+          type="number"
+          placeholder="Amount"
+          value={amount}
+          onChange={(e) => setAmount(e.target.value)}
+        />
+        <button onClick={addExpense}>Add Expense</button>
+      </div>
+
+      <div className="expense-list">
+        <h2>Expenses</h2>
+        <ol>
+          {expenses.map((expense) => (
+            <li key={expense.id}>
+              {expense.name}: ${expense.amount.toFixed(2)}
+              <button onClick={() => deleteExpense(expense.id)}>Delete</button>
+            </li>
+          ))}
+        </ol>
+      </div>
+	  
     </div>
   );
 }
 
 export default App;
+
